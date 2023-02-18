ffa717ca7a6cf413b6c16679aba2f3fade3d59db
kareemyasser8 <kareem.yasserr@gmail.com>
2023-02-18 10:41:00 +0200

    filter in find

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,22 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false) 
 
 mongoose.connect('mongodb://127.0.0.1/playground').then(
     () => console.log("Connected to Mongo DB...")
 ).catch(
     (err) => console.log("Could not connect to MongoDB...", err)
 )
-
-//async function ConnectToMongo(){
-//    try {
-//        await mongoose.connect('mongodb://127.0.0.1:27017/test');
-//      } catch (error) {
-//        handleError(error);
-//      }
-//}
-
-//ConnectToMongo();
 
 
 const courseSchema = new mongoose.Schema({
@@ -27,22 +17,42 @@ const courseSchema = new mongoose.Schema({
     isPublished: Boolean
 })
 
-//Classes and objects
-//Human, John
-
-const Course = mongoose.model('Course', courseSchema)
+const Course = mongoose.model('Course', courseSchema); //we get a Course class in our application
 
 async function createCourse() {
-
+    
     const course = new Course({
-        name: "Node.js Course",
-        author: "Kareem",
-        tags: ['node', 'backend'],
+        name: 'Redux Course',
+        author: 'Kareem',
+        tags: ['react', 'front-end'],
         isPublished: true
     })
 
+    //once we have a schema, we need to compile that in a model in order to have a class.
+    //then we create an object based on that class, and this object maps to a document in a mongo DB database
+
+
     const result = await course.save();
     console.log(result)
+
 }
 
-createCourse();
+async function getCourses(){
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than)
+    // gte  (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in)
+
+    const courses = await Course
+    .find({author: 'Kareem', isPublished: true })
+    .limit(10)
+    .sort({name: 1}) // 1 means ascending order, -1 means descending order
+    .select({name: 1,tags: 1})
+    console.log(courses);
+}
+
+getCourses();
